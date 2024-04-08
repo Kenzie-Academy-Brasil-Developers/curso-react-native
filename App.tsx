2067a164e44bf0dfa669c2933aacc8d562abf6b2
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-04-08 18:20:42 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Olá mundo!</Text>      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
