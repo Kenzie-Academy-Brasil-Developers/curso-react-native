306c1c9cf44577b6fedcc9df02dbe257726e8e78
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-04-26 15:26:43 -0300

    async storage

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,34 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function HomePage() {
+   const [isReady, setIsReady] = useState(false);
+   const [colorMode, setColorMode] = useState("LIGHT");
+
+   useEffect(() => {
+      if(!isReady){
+         async function load(){
+            const localColorMode = await AsyncStorage.getItem("@COLOR_MODE");
+            if(localColorMode) {
+               const parsedColorMode = JSON.parse(localColorMode);
+               setColorMode(parsedColorMode);
+            }
+            setIsReady(true);
+         }
+         load();
+      } else {
+         AsyncStorage.setItem("@COLOR_MODE", JSON.stringify(colorMode));
+      }      
+   }, [colorMode])
+
    return (
       <View>
-         <Text>Home Page</Text>
-         <View>
-            <Link href="/profile">Profile</Link>
-         </View>
+         <Text>{colorMode}</Text>
+         <Button
+            title="Modo de cor"
+            onPress={() => setColorMode(colorMode === "LIGHT" ? "DARK" : "LIGHT")}
+         />
       </View>
    );
 }
